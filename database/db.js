4757ff74fb8fa4c452fe271e1f484a15371e4117
MananThakur2003 <thakurmanan19012003@gmail.com>
2024-05-06 12:20:28 +0530

    update db

--- database/db.js
+++ database/db.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const Connection = async (username, password) => {
-    const URL = "mongodb+srv://thakursahab:manan1234@shri-ram.j6q5tkm.mongodb.net/";
+    const URL = `mongodb+srv://${username}:${password}@shri-ram.j6q5tkm.mongodb.net/`;
     try {
         await mongoose.connect(URL, { useNewUrlParser: true })
         console.log('Database connected successfully');
